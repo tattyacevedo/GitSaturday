b07f18ea9b042696b91fe8dfa491684f689b0cda
Tatiana <Tatiana@10.90.7.81>
2021-10-02 11:07:23 -0400

    made changes

--- src/saturday/DemoSaturday.java
+++ src/saturday/DemoSaturday.java
@@ -2,13 +2,22 @@ package saturday;
 
 public class DemoSaturday {
 	
+	 public static void addNum (int a, int b) {
+		 System.out.println(a+b);
+		 
+	 }
+	
 	public static void main (String [] args) {
 		
 		System.out.println("Saturday Demo...");
-		//this is coming from gitHub
-		int a = 10;
-		int b = 20;
+		
+		//changes made by Tati
+		
 		
 	}
+	
+	
 
 }
+
+
